71ac062ae6b824833a02cec3e84938027d3da6a6
jrogde <jon.rogde@gmail.com>
2015-02-03 19:13:00 +0100

    removed debug log

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(request, response) {
-  response.send(cool());
+  response.send("Whaleatory");
 });
 
 app.listen(app.get('port'), function() {
