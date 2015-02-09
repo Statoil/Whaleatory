a8008128b85f62fe9a76cfde7d90ed92df434e10
jrogde <jon.rogde@gmail.com>
2015-02-09 13:52:37 +0100

    different configuration for test and production

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var app = express();
 //MongoDB setup
 var mongo = require('mongodb');
 var monk = require('monk');
-//var db = monk('localhost:27017/whaleatorydb');
-var db = monk(process.env.MONGOLAB_URI);
+var db = monk('localhost:27017/whaleatorydb');
+//var db = monk(process.env.MONGOLAB_URI);
+
+var environment = process.env.ENV || 'test';
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -14,23 +16,21 @@ app.get('/', function(request, response) {
 });
 
 app.get('/userlist', function(request, response){
-
-  //response.send("Userlistpage");
-  ///*
   request.db=db;
   var collection = db.get('usercollection');
   collection.find({},{},function(e,docs){
     response.send(docs.toString());
   });
-  //*/
 });
 
 app.get('/observation', function(request, response){
   request.db=db;
   var collection = db.get('observation');
-  collection.find({},{},function(e,docs){
-    response.send(docs);
-  })
+  response.send("Running in environment " + environment);
+  response.send("process.env.MONGOLAB_URI");
+ // collection.find({},{},function(e,docs){
+ //   response.send(docs);
+ // })
 })
 
 app.listen(app.get('port'), function() {
