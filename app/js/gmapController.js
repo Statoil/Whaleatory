0c8942d25772cb68c468d55a1f3bec4c02d5c0c2
SteinArne <sasiv@statoil.com>
2015-02-09 14:58:14 +0100

    whaletail

--- app/js/gmapController.js
+++ app/js/gmapController.js
@@ -11,7 +11,11 @@ angular.module('whaleatory').controller('gmapController', ['$scope','$http',
         //GMap related below
         $scope.map = {center: {latitude: 63.4394346, longitude: 10.481066 }, zoom: 14 };
         $scope.options = {scrollwheel: false};
-
+        var iconTail = 'img/whaletail.png';
+        $scope.markers = [
+            {id: 1, pos: {latitude: 63.4494346, longitude: 10.481066 }, icon: iconTail},
+            {id: 2, pos: {latitude: 63.4474346, longitude: 10.483066 }, icon: iconTail}
+        ];
     }
 
 ]);
