52505cdb6a2e65d87600b0606edfff198a8068a2
SteinArne <sasiv@statoil.com>
2015-02-09 14:29:32 +0100

    added path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 //MongoDB setup
@@ -6,10 +7,11 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/whaleatorydb');
 
+app.use(express.static(path.resolve(__dirname, '')));
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(request, response) {
-   response.send("Whaleatory" );
+   response.redirect('/app');
 });
 
 app.get('/userlist', function(request, response){
