cf4078097eb45eaae64339897e75d632774cbfb0
jrogde <jon.rogde@gmail.com>
2015-02-09 14:14:22 +0100

    isolated environments

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ var app = express();
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/whaleatorydb');
-//var db = monk(process.env.MONGOLAB_URI);
 
-var environment = process.env.ENV || 'test';
+//Hadle environment vars
+var dotenv = require('dotenv');
+dotenv.load();
+
+var db = monk(process.env.MONGOLAB_URI);
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -26,12 +29,12 @@ app.get('/userlist', function(request, response){
 app.get('/observation', function(request, response){
   request.db=db;
   var collection = db.get('observation');
-  response.send("Running in environment " + environment);
-  response.send("process.env.MONGOLAB_URI");
- // collection.find({},{},function(e,docs){
- //   response.send(docs);
- // })
+  response.send("Running in environment " + process.env.ENVIRONMENT + " - " + process.env.MONGOLAB_URI);
+//  collection.find({},{},function(e,docs){
+//    response.send(docs);
+//  })
 })
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
