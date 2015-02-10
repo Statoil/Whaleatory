2fbf873877a5b3edd26cac0e1636f819f570bb97
jrogde <jon.rogde@gmail.com>
2015-02-10 11:35:43 +0100

    added image on add observation page

--- app/js/observationController.js
+++ app/js/observationController.js
@@ -8,18 +8,17 @@ angular.module('whaleatory').controller('observationController', ['$scope','$htt
         $scope.species =
 	    [
 	        { id: 1, type: "Whale", name: "North Atlantic Right Whales", icon:"img/icon/whale_trans_small.png"},
-	        { id: 2, type: "Whale", name: "Fin Whale", icon:"img/icon/whale_trans_small.png", image:"app/img/images/FinWhale.jpg"},
+
+	        { id: 2, type: "Whale", name: "Fin Whale", icon:"img/icon/whale_trans_small.png", image:"img/images/FinWhale.jpg"},
 	        { id: 3, type: "Whale", name: "Humpback Whale", icon:"img/icon/whale_trans_small.png"},
 	        { id: 4, type: "Whale", name: "Killer Whale", icon:"img/icon/whale_trans_small.png"},
-	        { id: 5, type: "Whale", name: "Pygmy Sperm Whale", icon:"img/icon/whale_trans_small.png", image:"app/img/images/SpermWhale.jpg"},
+	        { id: 5, type: "Whale", name: "Pygmy Sperm Whale", icon:"img/icon/whale_trans_small.png", image:"img/images/SpermWhale.jpg"},
 	        { id: 6, type: "Dolphin", name: "Atlantic Spotted Dolphin", icon:"img/icon/dolphin_trans_small.png"},
 	        { id: 7, type: "Dolphin", name: "Bottlenose Dolphin", icon:"img/icon/dolphin_trans_small.png"},
-	        { id: 8, type: "Seal", name: "Northern Elephant Seal", icon:"img/icon/seal_trans_small.png", image:"app/img/images/NortherElephantSeal.jpg"},
+	        { id: 8, type: "Seal", name: "Northern Elephant Seal", icon:"img/icon/seal_trans_small.png", image:"img/images/NorthernElephantSeal.jpg"},
 	        { id: 9, type: "Seal", name: "Spotted Seal", icon:"img/icon/seal_trans_small.png"}
 	    ];  
 	    
-	    $scope.selectedSpecies = {};
-		
 		$scope.newObs = {pos: {latitude:"63.4474946", longitude:"10.481256"}, comment:"comment", species:"species", time:"time"};
         $scope.proceed = function( ) {
 	        // Simple POST request example (passing data) :
