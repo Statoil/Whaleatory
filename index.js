b4012f469230d948f1960baf07f9acbbe114cc99
jrogde <jon.rogde@gmail.com>
2015-02-10 13:48:03 +0100

    date formatting function

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ var bodyParser = require('body-parser');
 app.use( bodyParser.json() );       				// to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
+var getTime = function(d){
+	var curr_date = d.getDate()<10 ? "0" + d.getDate() : d.getDate();
+	var intMonth = parseInt(d.getMonth())+1;
+	var curr_month = intMonth<10 ? "0" + intMonth : intMonth;
+	var curr_year = d.getFullYear();
+	var curr_hour = d.getHours()<10 ? "0" + d.getHours() : d.getHours();
+	var curr_mins = d.getMinutes()<10 ? "0" + d.getMinutes() : d.getMinutes();
+	var curr_seconds = d.getSeconds()<10 ? "0" + d.getSeconds() : d.getSeconds();
+	return curr_date + "/" + curr_month + "/" + curr_year + " " + curr_hour + ":" + curr_mins + ":" + curr_seconds;
+}
+
 app.use(express.static(path.resolve(__dirname, '')));
 //Hadle environment vars
 var dotenv = require('dotenv');
@@ -35,7 +46,15 @@ app.get('/observation', function(request, response){
 app.post('/observation', function(request, response) {
     // Set our internal DB variable
     var observations = request.body.observations;
-
+    	console.log("setting time for observation")
+    for(var i=0; i<observations.length;i++){
+    	var obsTime = observations[i].time;
+    	console.log("original time [" + obsTime + "]");
+	    if(obsTime==='time' || obsTime===''){
+	    	colsole.log("setting new time");
+	    	observations[i].time=getTime();
+	    }
+	}
     var collection = db.get('observation');
     //TODO validate input
     collection.insert(observations, function (err, doc) {
@@ -49,18 +68,17 @@ app.post('/observation', function(request, response) {
 
 app.get('/mock', function(request, response){
   request.db=db;
-  var d1 = new Date().toJSON();
+  var dateString = getTime(new Date());
   var observations = [
-	  	{pos: {latitude: 63.4494346, longitude: 10.481066 }, "species" : {"id" : 2, type : "Whale", name : "Fin Whale", icon : "img/icon/whale_trans_small.png", image:"img/images/FinWhale.jpg"}, comment: 'Johnny Longtail 1969'},
-	    {pos: {latitude: 63.4474346, longitude: 10.483066 }, "species" : {"id" : 5, type: "Whale", name: "Pygmy Sperm Whale", icon:"img/icon/whale_trans_small.png", image:"img/images/SpermWhale.jpg"}, comment: 'Peter Shorttail 1974'},
-	    {pos: {latitude: 63.4394346, longitude: 10.481466 }, "species" : {"id" : 8, type: "Seal", name: "Northern Elephant Seal", icon:"img/icon/seal_trans_small.png", image:"img/images/NorthernElephantSeal.jpg"}, comment: 'Lenny WideTail 1989'}
+	  	{pos: {latitude: 63.4494346, longitude: 10.481066 }, "species" : {"id" : 2, type : "Whale", name : "Fin Whale", icon : "img/icon/whale_trans_small.png", image:"img/images/FinWhale.jpg"}, comment: 'Johnny Longtail 1969', time: dateString},
+	    {pos: {latitude: 63.4474346, longitude: 10.483066 }, "species" : {"id" : 5, type: "Whale", name: "Pygmy Sperm Whale", icon:"img/icon/whale_trans_small.png", image:"img/images/SpermWhale.jpg"}, comment: 'Peter Shorttail 1974', time: dateString},
+	    {pos: {latitude: 63.4394346, longitude: 10.481466 }, "species" : {"id" : 8, type: "Seal", name: "Northern Elephant Seal", icon:"img/icon/seal_trans_small.png", image:"img/images/NorthernElephantSeal.jpg"}, comment: 'Lenny WideTail 1989', time: dateString}
     ];
   
   var collection = db.get('observation');
   collection.insert(observations);
   response.send("Added entries to the database" + observations);
 })
-
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
