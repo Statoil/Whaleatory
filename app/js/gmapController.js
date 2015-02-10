010f5d5cacfb832f5e07357697593aacfa1252e3
SteinArne <sasiv@statoil.com>
2015-02-10 09:24:37 +0100

    realtime update

--- app/js/gmapController.js
+++ app/js/gmapController.js
@@ -3,19 +3,41 @@ angular.module('whaleatory').controller('gmapController', ['$scope','$http',
 
     function ($scope,$http) {
 
+        var refreshRate = 1000;
         var vm = this;
         var iconTail = 'img/whaletail.png';
-        $scope.markers = [];
 
-
-        var promise = $http.get('/observation-mock').then(function (response) {
-            $scope.markers = response.data;
-
-        });
+        $scope.liveData = {};
 
         //GMap related below
         $scope.map = {mapTypeId: 'SATELLITE', center: {latitude: 63.4394346, longitude: 10.481066 }, zoom: 14 };
         $scope.options = {scrollwheel: false};
+
+        //// LIVE UPDATES BELOW
+        $scope.onTimeout = function () {
+            var refresh = {};
+            $http.get('/observation').
+                success(function(data, status, headers, config) {
+                    refresh.updated = data;
+                    $scope.liveData = refresh;
+                }).
+                error(function(data, status, headers, config) {
+                });
+            return refresh;
+        };
+
+        $scope.liveData  = $scope.onTimeout();  //Timeout function
+
+        $scope.markers = [];
+        $scope.$watch('liveData', function () {
+            $timeout($scope.onTimeout, refreshRate);
+            if (typeof $scope.liveData.updated != 'undefined') {
+                for (var i = 0; i < $scope.liveData.updated.length; i++) {
+                    var marker = $scope.liveData.updated[i];
+                    console.log(JSON.stringify(marker));
+                }
+            }
+        });
 
     }
 
